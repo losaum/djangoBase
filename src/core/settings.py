8a78e24ad2ce946eb1e7bda70205d013949bedaf
Thiago Berticelli Ló <losaum@gmail.com>
2023-07-04 09:21:22 -0300

    run scripts

--- src/core/settings.py
+++ src/core/settings.py
@@ -170,6 +170,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [ #adcicionado L6
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
---
+++ src/scripts/exemplo.py
@@ -0,0 +1,10 @@
+# from polls.models import Question
+
+# def run():
+#     # Fetch all questions
+#     questions = Question.objects.all()
+#     # Delete questions
+#     questions.delete()
+
+## Para executar:
+## python manage.py runscript delete_all_questions
